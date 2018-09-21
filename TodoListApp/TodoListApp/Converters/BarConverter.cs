e463f88d9925b3a1fd48de0bf6192eedcece5e75
Sergio Pereira <jose.mendes@sapium.com.br>
2018-09-21 17:45:08 -0300

    layout improvements

---
+++ TodoListApp/TodoListApp/Converters/BarConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using Xamarin.Forms;
+using System.Globalization;
+
+namespace TodoListApp.Converters
+{
+    class BarConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case "Alta":
+                    return Color.Red;
+                case "Média":
+                    return Color.Blue;
+                default:
+                    return Color.Green;
+            }
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
